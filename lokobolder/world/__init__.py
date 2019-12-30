fb70cfc61ee3ef42882b0ab88a2526e11974db36
wyattscarpenter <wyattscarpenter@gmail.com>
2019-12-29 18:30:58 -0800

    docstring for hardcoded world

--- lokobolder/world/__init__.py
+++ lokobolder/world/__init__.py
@@ -6,8 +6,7 @@ world = Blueprint('world', __name__)
 
 @world.route('/<int:x>/<int:y>', methods=['GET'])
 def get_by_coordinates(x, y):
-
-    # Return the world chunk at the given coordinates.
+"""Return the world chunk at the given coordinates."""
 
     # To-do: Query database for the correct chunk
 
