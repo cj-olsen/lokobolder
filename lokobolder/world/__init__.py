c36c257cc9d3c697afb154dccdece8e2abfb4fc9
wyattscarpenter <wyattscarpenter@gmail.com>
2020-01-04 16:10:41 -0800

    correct format of function

--- lokobolder/world/__init__.py
+++ lokobolder/world/__init__.py
@@ -6,11 +6,8 @@ world = Blueprint('world', __name__)
 
 @world.route('/<int:x>/<int:y>', methods=['GET'])
 def get_by_coordinates(x, y):
-"""Return the world chunk at the given coordinates."""
-
-    # To-do: Query database for the correct chunk
-
-    # To-do: Return jsonified chunk data
+    """Return the world chunk at the given coordinates."""
+    # TODO: Query database for the correct chunk, then jsonify and return it.
     smallworld = [["DOOM","Desert","Forest"],
                   ["Town","Lake","Tundra"],
                   ["Shipyards","Boneyards","Gokart Tracks"]]
